92bae711b21498246ba157ff10acd1094ed2b8e3
Raymond Hammarling <raymond@hammarlings.se>
2016-02-20 22:20:21 +0100

    Allow any bot name for update-on-hook script

--- scripts/update-on-hook.js
+++ scripts/update-on-hook.js
@@ -1,7 +1,7 @@
 var cp = require('child_process');
 
 module.exports = function(robot) {
-    robot.router.post('/Kurisu/update-hook', function(req, res) {
+    robot.router.post('/' + robot.name + '/update-hook', function(req, res) {
         res.end('Update hook received!!');
         console.log('Running ' + process.env.HUBOT_HOOK_UPDATE_CMD);
         cp.exec(process.env.HUBOT_HOOK_UPDATE_CMD);
